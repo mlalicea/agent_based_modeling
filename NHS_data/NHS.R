a88be915946cee97a52b0f11a4b380c680bc0c3e
Monica Alicea <55210154+mlalicea@users.noreply.github.com>
2020-10-07 14:38:00 -0400

    Delete NHS.R

--- NHS_data/NHS.R
+++
@@ -1,5 +0,0 @@
-install.packages("haven", dependencies = TRUE)
-
-setwd("/Users/Monica/Google Drive/ABM/defacto_description")
-haven::read_('/Users/Monica/Google Drive/ABM/NHS_data/NPIR7HDT/NPIR7HFL.DO)
-
